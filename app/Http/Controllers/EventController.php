428005b3d8f87da3dc01d0db310bdef0de29dcfb
hangzili <hangzili@163.com>
2019-10-14 16:22:47 +0800

    'b'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Tools\Tools;
 class EventController extends Controller
 {
+	public $tools;
+	public $request;
+	public function __construct($tools,$request)
+	{
+		$this->tools = $tools;
+		$this->request = $request;
+	}
     public function event()
     {
     	echo $_GET['echostr'];
