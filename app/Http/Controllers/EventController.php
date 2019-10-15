02a3c612b2261029d0172e4700790719de6dd6f2
hangzili <hangzili@163.com>
2019-10-15 09:02:50 +0800

    n

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -16,6 +16,8 @@ class EventController extends Controller
 
     public function event()
     {
+    	// $a=storage_path('logs/wecha/');
+    	// dd($a);
     	$info = file_get_contents("php://input");
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",FILE_APPEND);
         file_put_contents(storage_path('logs/wechat/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
