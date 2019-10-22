f62576b88a778e438903248337a37295da39bdf5
hangzili <hangzili@163.com>
2019-10-22 10:44:13 +0800

    1232

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,11 +26,11 @@ class EventController extends Controller
         	//将对象转化为数组格式23
         $xml_arr = (array)$xml_obj;
         // dump($xml_arr);
-        // if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
-        //     $wechat_user = $this->tools->get_wechat_user($xml_arr['FromUserName']);
-        //     $msg = '你好'.$wechat_user['nickname'].'，欢迎关注我的宝贝！';
-        //     echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
-        // }
+        if($xml_arr['MsgType'] == 'event' && $xml_arr['Event'] == 'subscribe'){
+            $wechat_user = $this->tools->get_wechat_user($xml_arr['FromUserName']);
+            $msg = '你好'.$wechat_user['nickname'].'，欢迎关注我的宝贝！';
+            echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+        }
         if($xml_arr['MsgType'] == 'text' && $xml_arr['Content'] == '你好'){
             $wechat_user = $this->tools->get_wechat_user($xml_arr['FromUserName']);
             $msg = '我好你也好';
