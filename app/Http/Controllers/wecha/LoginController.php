8157adc606e596323c14b31bd957d499de0448fd
hangzili <hangzili@163.com>
2019-10-28 14:43:39 +0800

    'sdfs'

--- app/Http/Controllers/wecha/LoginController.php
+++ app/Http/Controllers/wecha/LoginController.php
@@ -10,8 +10,8 @@ class LoginController extends Controller
     //授权登陆
     public function wecha_login()
     {
-    	$urls=urlencode(env('APP_URL').'/wecha/kess_add');
-    	$url= 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcc4c342a42f5b788&redirect_uri=http://http://47.94.20.198/wecha/kess_add&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+    	$urls=urlencode(env('APP_URL').'/wechat/kess_add');
+    	$url= 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcc4c342a42f5b788&redirect_uri=http://http://47.94.20.198/wechat/kess_add&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
     	header('Location:'.$url);
     }
     //授权登陆后获取关注着的基本信息
