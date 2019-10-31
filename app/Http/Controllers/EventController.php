56976231fb28a9c6593a03345bcfb70e82c9bf63
hangzili <hangzili@163.com>
2019-10-31 09:06:12 +0800

    'ewr'

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -40,9 +40,19 @@ class EventController extends Controller
             $msg = '白伟';
             echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
         }
+        //回复图片消息
         if($xml_arr['MsgType'] == 'text' && $xml_arr['Content'] == '2'){
-            $msg = 'www.a.cn/storage/wechat/image/15712324757034.jpg';
-            echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+//            $msg = 'www.a.cn/storage/wechat/image/15712324757034.jpg';
+//            echo "<xml><ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName><FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName><CreateTime>".time()."</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[".$msg."]]></Content></xml>";
+            echo "<xml>
+                  <ToUserName><![CDATA[".$xml_arr['FromUserName']."]]></ToUserName>
+                  <FromUserName><![CDATA[".$xml_arr['ToUserName']."]]></FromUserName>
+                  <CreateTime>1571232477</CreateTime>
+                  <MsgType><![CDATA[image]]></MsgType>
+                  <Image>
+                    <MediaId><![CDATA[1]]></MediaId>
+                  </Image>
+                </xml>";
         }
 
 
